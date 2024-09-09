e8ef1e066d4aee11a8cc218944f61721a06bb275
KoushikBinary01 <159457401+KoushikBinary01@users.noreply.github.com>
2024-09-09 20:34:43 +0530

    Time: 78 ms (64.90%) | Memory: 100.3 MB (6.73%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -15,3 +15,10 @@ public:
         return mat;
     }
 };
+
+static const auto speedup = []() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return 0;
+}();
