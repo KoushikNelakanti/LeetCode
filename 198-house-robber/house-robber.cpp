5a7257460a15e5ef08bcdfe58ffca548a37e2b75
KoushikBinary01 <159457401+KoushikBinary01@users.noreply.github.com>
2024-09-14 11:43:43 +0530

    Time: 7 ms (32.06%) | Memory: 12.8 MB (5.01%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int getMax(vector<int> nums,int index,unordered_map <int,int>& mp){
+        if(index>=nums.size()) return 0;
+        if(mp.find(index)!=mp.end()) return mp[index];
+        mp[index] = nums[index]+max(getMax(nums,index+2,mp),getMax(nums,index+3,mp));
+        return mp[index];
+    }
+    int rob(vector<int>& nums) {
+        unordered_map<int,int> mp;
+       int a = getMax(nums,0,mp);
+       int b = getMax(nums,1,mp);
+       cout<<a<<endl;
+       cout<<b<<endl;
+       return max(a,b);
+    }
+};
