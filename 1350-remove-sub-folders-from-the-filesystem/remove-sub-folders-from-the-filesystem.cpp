d97ecb48ecda8fb03adb6d4af3d888418f6eb1ed
KoushikBinary01 <159457401+KoushikBinary01@users.noreply.github.com>
2024-10-25 20:56:13 +0530

    Time: 528 ms (5.22%) | Memory: 203.8 MB (9.62%) - LeetSync

---
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    static bool lenSort(string a,string b){
+        return a.size()<b.size();
+    }
+    vector<string> removeSubfolders(vector<string>& folder) {
+        sort(folder.begin(),folder.end(),lenSort);
+        unordered_map <string,int> mp;
+        vector<string> result;
+        for(int i = 0;i < folder.size();i++){
+            string res = "";
+            for(int j = 0;j < folder[i].size();j++){
+                res+=folder[i][j];
+                if(res[res.size()]!='/'){
+                    if(mp[res]){
+                        if(j+1==folder[i].size() || folder[i][j+1]=='/') break;
+                    }
+                }
+            }
+            if(mp[res]==0) {
+                mp[res]++;
+                result.push_back(res);
+            }
+        }
+        return result;
+    }
+};
