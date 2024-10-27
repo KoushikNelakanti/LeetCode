ba206005adb4af0effe8c1a95fb165bd6ad33898
KoushikBinary01 <159457401+KoushikBinary01@users.noreply.github.com>
2024-10-27 09:14:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (92.79%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string s = "";
+            while(num>=1000){
+                num-=1000;
+                s+='M';
+            }
+            if(num>=900){
+                num-=900;
+                s+='C';
+                s+='M';
+            }
+            while(num>=500){
+                num-=500;
+                s+='D';
+            }
+            if(num>=400){
+                num-=400;
+                s+='C';
+                s+='D';
+            }
+            while(num>=100){
+                num-=100;
+                s+='C';
+            }
+            if(num>=90){
+                num-=90;
+                s+='X';
+                s+='C';
+            }
+            while(num>=50){
+                num-=50;
+                s+='L';
+            }
+            if(num>=40){
+                num-=40;
+                s+='X';
+                s+='L';
+            }
+            while(num>=10){
+                num-=10;
+                s+='X';
+            }
+            if(num>=9){
+                num-=9;
+                s+='I';
+                s+='X';
+            }
+            while(num>=5){
+                num-=5;
+                s+='V';
+            }
+            if(num>=4){
+                num-=4;
+                s+='I';
+                s+='V';
+            }
+            while(num>=1){
+                num--;
+                s+='I';
+            }
+            return s;
+    }
+};
