303e2153d11ffde9e774cdb5d93f04633368bb94
KoushikBinary01 <159457401+KoushikBinary01@users.noreply.github.com>
2024-11-01 22:03:10 +0530

    Time: 4 ms (5.91%) | Memory: 29 MB (5.20%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        k%=nums.size();
+        vector<int> v;
+        for(int i = nums.size()-k;i<nums.size();i++){
+            v.push_back(nums[i]);
+        }
+        for(int i = 0;i < nums.size()-k;i++){
+            v.push_back(nums[i]);
+        }
+        for(int i = 0;i < nums.size();i++){
+            nums[i]=v[i];
+        }
+    }
+};
