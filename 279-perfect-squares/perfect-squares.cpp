e052521438a5f7213ed0783c4d9ac2e5d64c56fc
KoushikBinary01 <159457401+KoushikBinary01@users.noreply.github.com>
2024-11-30 08:17:43 +0530

    Time: 1001 ms (5.06%) | Memory: 45 MB (13.16%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+vector<int> v;
+unordered_map<int,int> mp;
+    int getMinNum(int n){
+    if(n==0) return 0;
+    else if(n<0) return 1e9;
+    if(mp[n]) return mp[n];
+    mp[n]=1e9;
+    for(int i = v.size()-1;i>=0;i--){
+        mp[n] = min(mp[n],1+getMinNum(n-v[i]));
+    }
+    return mp[n];
+}
+    int numSquares(int n) {
+        for(int i = 1;i*i<=n;i++){
+            v.push_back(i*i);
+        }
+        return getMinNum(n);
+    }
+};
