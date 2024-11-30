efa7b2d19148da52e3df2c606fe1241ddfdfa709
KoushikBinary01 <159457401+KoushikBinary01@users.noreply.github.com>
2024-11-30 08:20:48 +0530

    Time: 899 ms (5.06%) | Memory: 46.2 MB (12.43%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -6,7 +6,7 @@ unordered_map<int,int> mp;
     if(n==0) return 0;
     if(mp[n]) return mp[n];
     mp[n]=1e9;
-    for(int i = v.size()-1;i>=0;i--){
+    for(int i = 0;i < v.size();i++){
         if(v[i]<=n) mp[n] = min(mp[n],1+getMinNum(n-v[i]));
     }
     return mp[n];
