527fd0e5b9824805fd1cb52b02aa600846d6a13f
KoushikBinary01 <159457401+KoushikBinary01@users.noreply.github.com>
2024-12-20 22:57:35 +0530

    Time: 419 ms (5.20%) | Memory: 90.2 MB (5.07%) - LeetSync

---
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int flag = 0,index = 0;
+    void levelOrder(TreeNode * root, vector<int>& order){
+        queue<TreeNode*> q;
+        TreeNode * temp = root;
+        TreeNode * t = new TreeNode(-1);
+        q.push(root);
+        while(q.size()){
+            q.push(NULL);
+            while(q.front() && q.front()->val!=-1){
+                if(flag==0) order.push_back(q.front()->val);
+                else q.front()->val=order[index++];
+                if(q.front()->left){
+                    q.push(q.front()->left);
+                }
+                else q.push(t);
+                if(q.front()->right){
+                    q.push(q.front()->right);
+                }
+                else q.push(t);
+                q.pop();
+            }
+            while(q.size() && (q.front()==NULL || q.front()->val==-1)){
+                q.pop();
+            }
+            if(flag==0) order.push_back(-1);
+            else index++;
+
+        }
+    }
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        vector<int> order;
+        levelOrder(root,order);
+        cout<<order.size()<<endl;
+        int prev = -1,next = -1,count = 0;
+        for(int i = 0;i < order.size();i++){
+            if(order[i]==-1){
+               next = i;
+               if(count%2){
+                reverse(order.begin()+prev+1,order.begin()+next);
+               }
+               prev = i;
+                count++;
+            }
+        }
+        for(int i = 0;i < order.size();i++){
+            cout<<order[i]<<" ";
+        }
+        flag = 1;
+        levelOrder(root,order);
+        return root;
+    }
+};
