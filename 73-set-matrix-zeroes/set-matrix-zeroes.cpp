22220f25da1ddbf5be3ba927da6071cd0359f767
KoushikBinary01 <159457401+KoushikBinary01@users.noreply.github.com>
2025-03-21 22:29:47 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (95.83%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
-    void setRow(int index,vector<vector<int>>& matrix){
-        int ele = -10101;
-        for(int i = 0;i < matrix[index].size();i++){
-            if(matrix[index][i]) matrix[index][i]=ele;
-        }
-    }
-    void setColumn(int index,vector<vector<int>>& matrix){
-        int ele = -10101;
-        for(int i = 0;i < matrix.size();i++){
-            if(matrix[i][index]) matrix[i][index]=ele;
-        }
-    }
     void setZeroes(vector<vector<int>>& matrix) {
-        int ele = -10101;
+        vector<int> row(matrix.size(),0);
+        vector<int> col(matrix[0].size(),0);
         for(int i = 0;i < matrix.size();i++){
             for(int j = 0;j < matrix[i].size();j++){
-                if(!matrix[i][j]){
-                    setRow(i,matrix);
-                    setColumn(j,matrix);
+                if(matrix[i][j]==0){
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
+
         for(int i = 0;i < matrix.size();i++){
             for(int j = 0;j < matrix[i].size();j++){
-                if(matrix[i][j]==ele){
+                if(row[i]|| col[j]){
                     matrix[i][j]=0;
                 }
             }
         }
-        
     }
 };
