f4188b67f7960fc9b67e40156678e827e1400fdd
KoushikNelakanti <159457401+KoushikNelakanti@users.noreply.github.com>
2025-06-10 22:46:14 +0530

    Time: 3 ms (4.37%) | Memory: 10.9 MB (22.36%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.c
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.c
@@ -9,8 +9,8 @@ struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
     if(list1==list2 && list1==NULL) return list1;
     else if(list1==NULL) return list2;
     else if(list2==NULL) return list1;
-    struct ListNode * head = (struct ListNode *)malloc(sizeof(struct ListNode)) ,*t1 = list1,*t2 = list2;
-    struct ListNode *t = head;
+    struct ListNode * head = (struct ListNode *)malloc(sizeof(struct ListNode)) ,*t1 = list1,*t2 = list2,*t;
+    t = head;
     head->val = 10;
     while(t1 && t2){
         struct ListNode * nn =  (struct ListNode *)malloc(sizeof(struct ListNode));
