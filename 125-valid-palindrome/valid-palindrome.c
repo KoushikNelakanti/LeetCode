11b6ce4d1cdfdf6fca9755d41e64c7d8e90a3f51
KoushikNelakanti <159457401+KoushikNelakanti@users.noreply.github.com>
2025-06-13 13:51:07 +0530

    Time: 43 ms (6.85%) | Memory: 8.4 MB (90.39%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.c
@@ -0,0 +1,18 @@
+bool ischar(char ch){
+    return (ch>='a' && ch<='z') || (ch>='A' && ch<='Z') ||(ch>='0' && ch<='9');
+}
+bool isPalindrome(char* s) {
+    int i = 0,j = strlen(s)-1;
+    while(i<j){
+       if(i<j && !ischar(s[i])) i++;
+       if(j>i && !ischar(s[j])) j--;
+       if(j>i && ischar(s[i]) && ischar(s[j])){
+        char a = tolower(s[i]),b = tolower(s[j]);
+        printf("%c %c\n",a,b);
+        if(a!=b) return false;
+        i++;
+        j--;
+       }
+    }
+    return true;
+}
