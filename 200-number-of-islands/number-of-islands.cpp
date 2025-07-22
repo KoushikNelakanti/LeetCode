efaf7b9fcf750ee11d8ab73c52a03fb7185cc71c
KoushikNelakanti <159457401+KoushikNelakanti@users.noreply.github.com>
2025-07-22 14:32:59 +0530

    Time: 36 ms (18.08%) | Memory: 23 MB (38.23%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
+    bool isValid(int x,int y,vector<vector<char>>& grid){
+        return !(x<0 || y<0 || x>=grid.size() || y>=grid[0].size() || grid[x][y]=='0' || grid[x][y]=='2');
+    }
     void dfs(int i,int j,vector<vector<char>>& grid){
-        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size()) return;
-        if(grid[i][j]=='2' || grid[i][j]=='0') return;
-        grid[i][j]='2';
-       dfs(i+1,j,grid);
-       dfs(i-1,j,grid);
-       dfs(i,j+1,grid);
-       dfs(i,j-1,grid);
+       stack<pair<int,int>> st;
+       pair<int,int> p;
+       p.first = i;
+       p.second = j;
+       st.push(p);
+       while(st.size()){
+            pair<int,int> top = st.top();
+            st.pop();
+            int x = top.first,y = top.second;
+            grid[x][y] = '2';
+            if(isValid(x+1,y,grid)){
+                st.push({x+1,y});
+            }
+            if(isValid(x-1,y,grid)){
+                st.push({x-1,y});
+            }
+            if(isValid(x,y+1,grid)){
+                st.push({x,y+1});
+                
+            }
+            if(isValid(x,y-1,grid)){
+                st.push({x,y-1});
+            }
+       }
     }
     int numIslands(vector<vector<char>>& grid) {
         int islands = 0;
