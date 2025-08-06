96836b4d2f3402b928109f17a589e95f421b87a8
KoushikNelakanti <159457401+KoushikNelakanti@users.noreply.github.com>
2025-08-06 22:08:02 +0530

    Time: 301 ms (5.75%) | Memory: 23.5 MB (27.83%) - LeetSync

---
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    bool isInvalid(int i,int j,vector<vector<int>>& mat){
+        return (i<0 || j<0 || i>=mat.size() || j>=mat[i].size());
+    }
+    int getPathLength(int i,int j,vector<vector<int>>& matrix,vector<vector<int>>& mp){
+        if(isInvalid(i,j,matrix)){
+            return -1000;
+        }
+        if(mp[i][j]) return mp[i][j];
+        int a = 1,b=1,c=1,d=1;
+        if(!isInvalid(i+1,j,matrix) && matrix[i][j]<matrix[i+1][j]){
+            a = 1+getPathLength(i+1,j,matrix,mp);
+        }
+        if(!isInvalid(i-1,j,matrix) && matrix[i][j]<matrix[i-1][j]){
+            b = 1+getPathLength(i-1,j,matrix,mp);
+        }
+        if(!isInvalid(i,j+1,matrix) && matrix[i][j]<matrix[i][j+1]){
+            c = 1+getPathLength(i,j+1,matrix,mp);
+        }
+        if(!isInvalid(i,j-1,matrix) && matrix[i][j]<matrix[i][j-1]){
+            d = 1+getPathLength(i,j-1,matrix,mp);
+        }
+        mp[i][j]=max(a,max(b,max(c,d)));
+        return mp[i][j];
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        vector<vector<int>> mp;
+        for(int i = 0;i < matrix.size();i++){
+            vector<int> v;
+            for(int j = 0;j < matrix[i].size();j++){
+                v.push_back(0);
+            }
+            mp.push_back(v);
+        }
+        int maxLen = 0;
+        for(int i = 0;i < matrix.size();i++){
+            for(int j = 0;j < matrix[i].size();j++){
+                int len;
+                if(!mp[i][j]) len = getPathLength(i,j,matrix,mp);
+                else len = mp[i][j];
+                maxLen = max(len,maxLen);
+                cout<<mp[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+        return maxLen;
+    }
+};
